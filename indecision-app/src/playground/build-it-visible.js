94181f1b9882fe8d050ec3de98f9decf9a7381b5
krishnan2098 <krishnan2098@gmail.com>
2018-05-27 23:51:34 +0530

    remove unnecessary code

--- indecision-app/src/playground/build-it-visible.js
+++ indecision-app/src/playground/build-it-visible.js
@@ -31,29 +31,3 @@ class VisibilityToggle extends React.Component {
 }
 
 ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
-// let visibility = false;
-//
-// const toggleVisibility = () => {
-//     visibility = !visibility;
-//     renderApp();
-// };
-//
-// const appRoot = document.getElementById('app');
-//
-// const renderApp = () => {
-//     const template = (
-//         <div>
-//             <h1>Visibility Toggle</h1>
-//             <button onClick={toggleVisibility}>{visibility? 'Hide Details' : 'Show Details'}</button>
-//             { visibility && (
-//                 <div>
-//                 <p>Now it is visible</p>
-//                 </div>
-//             )}
-//         </div>
-//     );
-//
-//     ReactDOM.render(template, appRoot);
-// };
-//
-// renderApp();
