928b90d910b448f57e47e4374ddd82e42d703ef6
krishnan2098 <krishnan2098@gmail.com>
2018-06-02 23:45:46 +0530

    Pass jsx as props-method 1

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,4 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const Layout = (props) => {
+    return (
+        <div>
+            <p>header</p>
+            {props.content}
+            <p>footer</p>
+        </div>
+    )
+};
+
+const template = (
+    <div>
+        <h1>Page title</h1>
+        <p>this is my page</p>
+    </div>
+);
+
+ReactDOM.render(<Layout content = {template} />, document.getElementById('app'));
