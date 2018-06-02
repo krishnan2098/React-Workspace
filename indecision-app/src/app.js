1273bf55c23acb4ef95f02d0cd52303690662ff2
krishnan2098 <krishnan2098@gmail.com>
2018-06-02 16:47:26 +0530

    Test imports of npm modules like react and reactDOm

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -5,7 +5,16 @@
 // console.log(add(100, 23));
 // console.log(subtract(100, 50));
 
-import isSenior, { isAdult, canDrink } from "./person.js";
-console.log('Adult?', isAdult(100));
-console.log('Drink?', canDrink(100));
-console.log('Senior Citizen?', isSenior(1000));
+// import isSenior, { isAdult, canDrink } from "./person.js";
+// console.log('Adult?', isAdult(100));
+// console.log('Drink?', canDrink(100));
+// console.log('Senior Citizen?', isSenior(1000));
+
+// import validator from 'validator';
+// console.log(validator.isEmail('test@gmail.com'));
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = React.createElement('p', {}, 'Testing 123');
+ReactDOM.render( template, document.getElementById('app'));
