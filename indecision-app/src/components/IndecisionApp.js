3bb5bc5a172331b465fa3759dbb3525ceb8634e1
krishnan2098 <krishnan2098@gmail.com>
2018-06-02 22:46:15 +0530

    Create Indecision App component

---
+++ indecision-app/src/components/IndecisionApp.js
@@ -0,0 +1,92 @@
+import React from "react";
+import AddOption from './AddOption';
+import Header from './Header';
+import Action from './Action';
+import Options from './Options';
+
+class IndecisionApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+        this.handlePick = this.handlePick.bind(this);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.handleDeleteOption = this.handleDeleteOption.bind(this);
+        this.state = {
+            options: []
+        };
+    }
+
+    componentDidMount() {
+        try {
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+            if(options) {
+                this.setState(() => ({ options }));
+            }
+            console.log('fetching data');
+        } catch(e) {
+            //Do nothing
+        }
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.options.length !== this.state.options.length) {
+            console.log('saving data');
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem('options', json);
+        }
+    }
+
+    componentWillUnmount() {
+        console.log("unmounting!");
+    }
+
+    handleDeleteOptions() {
+        this.setState(() => ({ options: [] }));
+    }
+
+    handleDeleteOption(optionToRemove) {
+        this.setState((prevState) => ({
+            options: prevState.options.filter((option) => optionToRemove !== option)
+        }));
+    }
+
+    handlePick() {
+        const randIndex = Math.floor(Math.random()*this.state.options.length);
+        alert(this.state.options[randIndex]);
+    }
+
+    handleAddOption(option) {
+        if(!option) {
+            return 'Field Cannot be left empty!';
+        } else if(this.state.options.indexOf(option) > -1) {
+            return 'This Option already exists';
+        }
+
+        this.setState((prevState) => ({ options: prevState.options.concat(option) }));
+    }
+
+    render() {
+        const subtitle = 'Put your hands in the life of a computer.';
+        return (
+            <div>
+                <Header subtitle={subtitle}/>
+                <Action
+                    hasOptions={this.state.options.length>0}
+                    handlePick={this.handlePick}
+                />
+                <Options
+                    options={this.state.options}
+                    handleDeleteOptions={this.handleDeleteOptions}
+                    handleDeleteOption={this.handleDeleteOption}
+                />
+                <AddOption
+                    handleAddOption={this.handleAddOption}
+                />
+            </div>
+        );
+    }
+}
+
+export default IndecisionApp;
