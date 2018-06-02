8a6e7c3311c6b28b0b56098beeb22809b06050a1
krishnan2098 <krishnan2098@gmail.com>
2018-06-02 22:46:47 +0530

    Create Option component

---
+++ indecision-app/src/components/Option.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Option = (props) => {
+    return (
+        <div>
+            <p>{props.optionText}</p>
+            <button onClick={() => {
+                props.handleDeleteOption(props.optionText);
+            }}>Remove</button>
+        </div>
+    );
+};
+
+export default Option;
