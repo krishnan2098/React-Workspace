0fd904b09f38bc7f4b539ffdf56737b337253cfb
krishnan2098 <krishnan2098@gmail.com>
2018-06-02 22:50:49 +0530

    Bootstrap app.js

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,20 +1,5 @@
-// import './utils.js'
-// import subtract, { square, add } from './utils.js'
-// console.log('this app,js file inside src directory!');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 50));
-
-// import isSenior, { isAdult, canDrink } from "./person.js";
-// console.log('Adult?', isAdult(100));
-// console.log('Drink?', canDrink(100));
-// console.log('Senior Citizen?', isSenior(1000));
-
-// import validator from 'validator';
-// console.log(validator.isEmail('test@gmail.com'));
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-const template = <p>This is jsx from webpack</p>;
-ReactDOM.render(template, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
