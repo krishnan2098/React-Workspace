3cd520d834756fe120434d608747c24d926f931e
krishnan2098 <krishnan2098@gmail.com>
2018-06-04 15:28:56 +0530

    refactor Header component to have an implicitly return value

--- indecision-app/src/components/Option.js
+++ indecision-app/src/components/Option.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-const Option = (props) => {
-    return (
+const Option = (props) => (
         <div>
             <p>{props.optionText}</p>
             <button onClick={() => {
                 props.handleDeleteOption(props.optionText);
             }}>Remove</button>
         </div>
-    );
-};
+);
 
 export default Option;
