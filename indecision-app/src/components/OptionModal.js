40b5923a82469582bbf1c6b3797d33f2bd3d19cd
krishnan2098 <krishnan2098@gmail.com>
2018-06-05 14:33:22 +0530

    Add modal styles and desktop breakpoint value ot the settings

--- indecision-app/src/components/OptionModal.js
+++ indecision-app/src/components/OptionModal.js
@@ -5,11 +5,13 @@ const OptionModal = (props) => (
     <Modal
         isOpen={!!props.selectedOption}
         onRequestClose={props.clearSelectedOption}
+        className='modal'
+        closeTimeoutMS={400}
         contentLabel="Selected Option"
     >
-        <h3>Selected Option</h3>
-        { props.selectedOption && <p>{ props.selectedOption }</p>}
-        <button onClick={props.clearSelectedOption}>Okay</button>
+        <h3 className='modal__title'>Selected Option</h3>
+        { props.selectedOption && <p className='modal__body'>{ props.selectedOption }</p>}
+        <button className='button' onClick={props.clearSelectedOption}>Okay</button>
     </Modal>
 );
 
