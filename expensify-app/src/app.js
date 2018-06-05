cf25a020d9be88c4778c05cec700e414c4068fb5
krishnan2098 <krishnan2098@gmail.com>
2018-06-05 20:56:41 +0530

    Render test component for the various routes

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import '../node_modules/normalize.css/normalize.css'
 import './styles/styles.scss';
 
+const ExpenseDashBoard = () => (
+    <div>
+        This is the DashBoard Component
+    </div>
+);
 
+const AddExpensePage = () => (
+    <div>
+        This is the Add Expense Component
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        This is the Edit Expense Component
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is the Help Component
+    </div>
+);
 
 const routes = (
     <BrowserRouter>
-        <Route path='/' component={} />
+        <div>
+            <Route path='/' component={ ExpenseDashBoard } exact={true} />
+            <Route path='/create' component={ AddExpensePage } />
+            <Route path='/edit' component={ EditExpensePage } />
+            <Route path='/help' component={ HelpPage } />
+        </div>
     </BrowserRouter>
 );
 
