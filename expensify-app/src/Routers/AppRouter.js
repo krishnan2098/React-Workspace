8f00ecc2470db9a892e361d25b69103e1fd2550f
krishnan2098 <krishnan2098@gmail.com>
2018-06-06 23:41:47 +0530

    Init Routers directory

---
+++ expensify-app/src/Routers/AppRouter.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import AddExpensePage from '../components/AddExpensePage.js';
+import EditExpensePage from '../components/EditExpensePage';
+import ExpenseDashBoard from '../components/ExpenseDashBoard';
+import Header from '../components/Header';
+import HelpPage from '../components/Header';
+import NotFoundPage from  '../components/NotFoundPage';
+
+const AppRouter = () => (
+    <BrowserRouter>
+        <div>
+            <Header/>
+            <Switch>
+                <Route path='/' component={ ExpenseDashBoard } exact={true} />
+                <Route path='/create' component={ AddExpensePage } />
+                <Route path='/edit' component={ EditExpensePage } />
+                <Route path='/help' component={ HelpPage } />
+                <Route component={ NotFoundPage } />
+            </Switch>
+        </div>
+    </BrowserRouter>
+);
+
+export default AppRouter;
