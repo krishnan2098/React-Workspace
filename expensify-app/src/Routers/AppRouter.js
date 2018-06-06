66eab932e5208fbf5d40feb17459046ca812c1c5
krishnan2098 <krishnan2098@gmail.com>
2018-06-06 23:55:04 +0530

    Refactored app.js and approuter.js to components

--- expensify-app/src/Routers/AppRouter.js
+++ expensify-app/src/Routers/AppRouter.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AddExpensePage from '../components/AddExpensePage.js';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashBoard from '../components/ExpenseDashBoard';
 import Header from '../components/Header';
-import HelpPage from '../components/Header';
+import HelpPage from '../components/HelpPage';
 import NotFoundPage from  '../components/NotFoundPage';
 
 const AppRouter = () => (
--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,42 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import AppRouter from './Routers/AppRouter';
 import '../node_modules/normalize.css/normalize.css'
 import './styles/styles.scss';
 
-const ExpenseDashBoard = () => (
-    <div>
-        This is the DashBoard Component
-    </div>
-);
-
-const AddExpensePage = () => (
-    <div>
-        This is the Add Expense Component
-    </div>
-);
-
-const EditExpensePage = () => (
-    <div>
-        This is the Edit Expense Component
-    </div>
-);
-
-const HelpPage = () => (
-    <div>
-        This is the Help Component
-    </div>
-);
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Route path='/' component={ ExpenseDashBoard } exact={true} />
-            <Route path='/create' component={ AddExpensePage } />
-            <Route path='/edit' component={ EditExpensePage } />
-            <Route path='/help' component={ HelpPage } />
-        </div>
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter/>, document.getElementById('app'));
