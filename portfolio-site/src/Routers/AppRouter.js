98b43f7a16ed8171382bb100e16fb05c1608a670
krishnan2098 <krishnan2098@gmail.com>
2018-06-07 01:35:36 +0530

    Set up portfolio site

---
+++ portfolio-site/src/Routers/AppRouter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import Header from '../components/Header';
+import NotFoundPage from  '../components/NotFoundPage';
+
+const AppRouter = () => (
+    <BrowserRouter>
+        <div>
+            <Header/>
+            <Switch>
+                <Route component={ NotFoundPage } />
+            </Switch>
+        </div>
+    </BrowserRouter>
+);
+
+export default AppRouter;
