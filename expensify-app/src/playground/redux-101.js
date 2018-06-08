67b06f5b2405ed7db9e7333282f3f915b3907d96
krishnan2098 <krishnan2098@gmail.com>
2018-06-08 22:07:31 +0530

    Apply Basic redux

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,7 +1,47 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count:0 }) => {
-    return state;
+const store = createStore((state = { count:0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+
+        case 'RESET':
+            return {
+                count: 0
+            };
+
+        default:
+            return state;
+    }
 });
+
+console.log(store.getState());
+
+//ACTION OBJECT FOR INCREMENTING
+const increment = {
+    type: 'INCREMENT'
+};
+
+//ACTION OBJECT FOR DECREMENTING
+const decrement = {
+    type: 'DECREMENT'
+};
+
+//ACTION OBJECT FOR RESETING
+const reset = {
+    type: 'DECREMENT'
+};
+
+//send action to store to change the state
+store.dispatch(increment);
+store.dispatch(reset);
+store.dispatch(decrement);
 
 console.log(store.getState());
