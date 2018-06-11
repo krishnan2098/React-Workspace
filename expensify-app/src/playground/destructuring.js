7a2d396ada743c947470a03847066677008ee0eb
krishnan2098 <krishnan2098@gmail.com>
2018-06-11 10:23:36 +0530

    destructure ES6 object

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -1,1 +1,30 @@
-console.log('destructuring');
+// const Person = {
+//     // name: 'Andrew',
+//     age: 27,
+//     location : {
+//         city: 'Philadelphia',
+//         temp: 88
+//     }
+// };
+//
+// const { name = 'Anonymous', age } = Person;
+//
+// console.log(`${ name } is ${ age }`);
+//
+// const { city, temp : temperature} = Person.location;
+//
+// if( city && temperature ) {
+//     console.log(`It's ${ temperature } in ${ city }`);
+// }
+
+const book = {
+    title: 'Ego is the Enemy',
+    author: 'Ryan Holiday',
+    publisher: {
+        name: 'Penguin'
+    }
+};
+
+const { name : publisherName = 'Self-Published' } = book.publisher;
+
+console.log( publisherName );
