4a836d968292fb758324a209ad248eeb0db07398
krishnan2098 <krishnan2098@gmail.com>
2018-06-11 20:32:36 +0530

    use combine reducers to pass multiple reducers to the store

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,5 +1,56 @@
 import { createStore, combineReducers } from 'redux';
 
+// REDUCERS SHOULD
+// 1. BE PURE FUNCTIONS NOT HAVING THEIR SCOPRE LIMITED TO THE FUNCTION ITSELF
+// 2. IT SHOULD NOT CHANGE THE STATE. RATHER IT SHOULD ACCESS THE STATE
+
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSES
+// SET-TEXT-FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+// EXPENSES_REDUCER
+
+const expensesReducerDefaultState = [];
+
+const expensesReducer = ( state = expensesReducerDefaultState, action ) => {
+    switch( action.type ) {
+        default:
+            return state;
+    }
+};
+
+// filter reducer
+
+const filterReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+
+const filterReducer = ( state = filterReducerDefaultState, action ) => {
+    switch( action.type ) {
+        default:
+            return state;
+    }
+};
+
+// store creation
+
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filter: filterReducer
+    })
+);
+
+console.log( store.getState() );
+
 const demoState = {
     expenses: [{
         id: 'adfvlndfvasdf',
@@ -10,7 +61,7 @@ const demoState = {
     }],
     filter: {
         text: 'rent',
-        sortBy: 'amount',
+        sortBy: 'amount', // date or amount
         startDate: undefined,
         endDate: undefined
     }
