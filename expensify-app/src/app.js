23ce4d347c7553856b20ad7bb17400783e1a908a
krishnan2098 <krishnan2098@gmail.com>
2018-06-30 23:57:48 +0530

    change naming convention for filter to filters

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './Routers/AppRouter';
-
 // Redux Store import
 import configureStore from './store/configureStore';
-
 //Redux action generators import
 import { addExpense } from "./actions/expenses";
 import { setTextFilter } from "./actions/filters";
-
 //Redux Selectors import
 import getVisibleExpenses from './selectors/expenses';
 
@@ -17,15 +15,22 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill' }));
+store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }));
 store.dispatch(addExpense({ description: 'Gas Bill' }));
 store.dispatch(setTextFilter('water'));
 
+setTimeout(() => {
+    store.dispatch(setTextFilter('Bill'))
+}, 3000);
 
 const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filter);
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
-// console.log(store.getState());
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
 
-ReactDOM.render(<AppRouter/>, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
